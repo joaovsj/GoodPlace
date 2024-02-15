a2f64b6ed5773048c4b8a41931e1edd94ed73642
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-15 20:56:02 -0300

    Somethings changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./pages/home/home.component').then((p)=> p.HomeComponent)
+        loadComponent: () => import('./pages/home/home.component').then(p=> p.HomeComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(p=> p.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./auth/signup/signup.component').then(p => p.SignupComponent)
     }
 ];
