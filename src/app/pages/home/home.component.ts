4843d351b115dd86cac7e70db65c518f2c02d547
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-14 23:28:38 -0300

    Comments updated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,20 +15,23 @@ export class HomeComponent {
   public comments = signal([
     {
       src: "assets/img/comment-one.jpg",
+      description: "Simplismente incrível, nem acredito que exista uma plataforma só para podermos falar sobre viagens e expriências...",
       person: {
-        name: "Eshiley",
+        name: "Eshiley Megan",
         job: "CEO Dantas"
       }
     },
     {
       src: "assets/img/comment-two.jpg",
+      description: "Eu tinha um pouco de receio no início, mas depois que comecei a usar não consegui mais parar, super recomendo!",
       person: {
-        name: "Eshiley",
-        job: "CEO Dantas"
+        name: "Richard Russel",
+        job: "Sócio-Executivo da Colins"
       }
     },
     {
       src: "assets/img/comment-three.jpg",
+      description: "Se quiser saber sobre algum lugar, pesquise aqui à respeito e te garanto que vai se sentir muito mais seguro quando for a sua vez de viajar. ",
       person: {
         name: "Eshiley",
         job: "CEO Dantas"
