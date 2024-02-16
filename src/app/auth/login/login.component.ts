1282f9011704032dde61118f852f6e892dfce983
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-16 18:43:29 -0300

    Animation of log in done

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,23 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [
+    trigger('init', [
+      state('show', style({
+        opacity: 1
+      })),
+
+
+      transition('* => show', animate('.6s ease'))
+    ])
+  ]
 })
 export class LoginComponent {
 
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,12 +1,24 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
+  animations: [
+    trigger('init', [
+      state('show', style({
+        opacity: 1,
+        // height: 'min-content'
+      })), // close state
+
+      transition('* => show', animate('.6s ease'))
+    ])
+  ]
 })
 export class SignupComponent {
-
+  
 }
