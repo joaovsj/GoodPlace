e535c3551864c51e8db31734caa6d1567ece61f2
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-19 20:59:18 -0300

    One container done

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+// Components
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
