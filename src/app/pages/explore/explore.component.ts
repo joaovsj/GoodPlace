a222e2edb76a87979f4289c011c95bca0a213678
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-22 20:41:53 -0300

    Some changes done

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 // Components
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { PlacesComponent } from './places/places.component';
+
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, PlacesComponent, ReactiveFormsModule],
   templateUrl: './explore.component.html',
-  styleUrl: './explore.component.scss'
+  styleUrl: './explore.component.scss', 
 })
 export class ExploreComponent {
+
+  #fb = inject(FormBuilder);
+
+  public filterSearch = this.#fb.group({
+    category: ['', Validators.required],
+    description: ['']
+  })
+
+
+  
 
 }
