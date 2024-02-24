6e8cc5b641515f042d4116a4e29483e0023bc035
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-24 19:38:50 -0300

    Animation of enter created

--- src/app/pages/explore/places/places.component.ts
+++ src/app/pages/explore/places/places.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,7 +6,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './places.component.html',
-  styleUrl: './places.component.scss'
+  styleUrl: './places.component.scss',
+  animations: [
+    trigger('visibility', [
+      state('visible', style({
+        opacity: 1,
+        pointerEvents: 'all' 
+      })),
+
+      transition('* => visible', animate('.6s ease'))
+    ])
+  ]
 })
 export class PlacesComponent {
 
