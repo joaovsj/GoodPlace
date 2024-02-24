60b5e0791b2b5b695f3e9dc760c96ce8a80869e5
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-24 18:03:45 -0300

    Way to get the value of input discovered

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, DoCheck, inject } from '@angular/core';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 // Components
 import { HeaderComponent } from '../../shared/components/header/header.component';
@@ -14,15 +14,28 @@ import { PlacesComponent } from './places/places.component';
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss', 
 })
-export class ExploreComponent {
 
+export class ExploreComponent implements DoCheck{
+  
   #fb = inject(FormBuilder);
+
 
   public filterSearch = this.#fb.group({
     category: ['', Validators.required],
     description: ['']
   })
 
+  
+  ngDoCheck(){
+    
+    const valueFilter = this.filterSearch.value;
+    console.log(valueFilter.category);
+    console.log('got in');
+  }
+
+  submit(){
+    console.log(this.filterSearch);
+  }
 
   
 
