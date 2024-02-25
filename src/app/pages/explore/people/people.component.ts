c7e355dffd88c9553fe359b752de8680f4cc3e1e
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-24 22:22:20 -0300

    New animation created

--- src/app/pages/explore/people/people.component.ts
+++ src/app/pages/explore/people/people.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { BoxListComponent } from '../../../shared/components/box-list/box-list.component';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-people',
   standalone: true,
   imports: [BoxListComponent],
   templateUrl: './people.component.html',
-  styleUrl: './people.component.scss'
+  styleUrl: './people.component.scss',
+  animations: [
+    trigger('visibility', [
+      state('visible', style({
+        opacity: 1,
+        pointerEvents: 'all' 
+      })),
+
+      transition('* => visible', animate('.6s ease'))
+    ])
+  ]
 })
 export class PeopleComponent {
 
