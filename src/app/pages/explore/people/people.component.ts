e37f14540c363a1558fbdd623e9f3b9a38826360
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-29 19:14:06 -0300

    CSS added

--- src/app/pages/explore/people/people.component.ts
+++ src/app/pages/explore/people/people.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BoxListComponent } from '../../../shared/components/box-list/box-list.component';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { BoxPostComponent } from '../../../shared/components/box-post/box-post.component';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [BoxListComponent],
+  imports: [BoxListComponent, BoxPostComponent],
   templateUrl: './people.component.html',
   styleUrl: './people.component.scss',
   animations: [
