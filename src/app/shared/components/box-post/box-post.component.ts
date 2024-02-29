5c2acd4c0aa39f6a6f6c635385863517aabad714
joaovsj <joaosilva.jesus90@gmail.com>
2024-02-29 19:10:46 -0300

    Component created

---
+++ src/app/shared/components/box-post/box-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './box-post.component.html',
+  styleUrl: './box-post.component.scss'
+})
+export class BoxPostComponent {
+
+}
