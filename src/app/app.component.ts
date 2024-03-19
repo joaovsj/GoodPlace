e899604f195a1b5ab45cdc3aef13501813884d4f
joaovsj <joaosilva.jesus90@gmail.com>
2024-03-18 22:18:17 -0300

    Toast has done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Component
+import { ToastComponent } from '@components/toast/toast.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToastComponent],
   template: `
+    <app-toast/>
     <router-outlet></router-outlet>
   `,
 })
--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,13 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToastComponent {
+
+  public toastMessage = signal<String>("Cadastro relizado com sucesso!"); 
+  public showsToast   = signal<boolean>(true);
 
 }
